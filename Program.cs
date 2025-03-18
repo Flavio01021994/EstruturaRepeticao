67d86178834783f3ee6ff87b2caee83ec19a08cd
FIC <fic@info.local>
2025-03-18 19:09:40 -0300

    EstruturaRepeticao

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// Estruturas Condicionais
+// Estruturas de repeticao 
+// repete uma ação 
+
+// imprimir todos os números de 1 ate 5 
+
+// enquanto - While
+//int numero = 1;
+
+//// Repete uma acao, ENQUANTO algo for verdade
+//while (numero < 6)
+//{
+//    Console.WriteLine(numero);
+//    // incremento
+//    numero = numero + 1;
+//    // numero++; atalho da linha de cima
+//}
+
+
+// faça - enquanto (Do/While)
+int idade; 
+
+do
+{
+    Console.WriteLine("Informe a idade:");
+    idade = int.Parse(Console.ReadLine());
+} while(idade !=22);
+
+//para - FOR
+// Quero imprimir os numeros do 1 ao 5
+// 1 cria uma variavel chamada i () indice
+// 2 - Condicao para continuar
+// 3 - incremento ao executar chaves (++)
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine(i);
+}
+
+//1. Contagem de 1 a 20**
+
+// Crie um programa que exiba os números de 1 a 20 na tela.
